9266a687b7851b75ca42fa208a9bddc8d7d24695
동아 <dongakim90@gmail.com>
2016-11-21 16:46:03 +0900

    make detailId

--- firstnodeserver/app.js
+++ firstnodeserver/app.js
@@ -8,6 +8,13 @@ var app = http.createServer(function(request, response){
 		response.write("This is HOME!!!");
 		response.end();
 	}
+
+	var detailId = request.url.replace("/", "");
+	if (detailId > 0) {
+		response.write(detailId);
+		response.end();
+	}
+
 }).listen(process.env.PORT || 3030);
 
 console.log("First Node Server is running at localhost");
