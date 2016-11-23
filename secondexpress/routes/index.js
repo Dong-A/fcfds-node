d66c11322797a56ddd925237ef0c12def0ae1d19
동아 <dongakim90@gmail.com>
2016-11-23 15:00:33 +0900

    ejs partials

--- secondexpress/routes/index.js
+++ secondexpress/routes/index.js
@@ -1,5 +1,14 @@
+// module.exports = function(app) {
+//   app.get('/', function(req, res) {
+//     res.send('Hello Second Express!');
+//   });
+// };
+// 이건 그냥 단순한 메시지만 보내는 것이다.
+// HTML 템플릿을 보내는 것이 아니다.
+
+
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    res.send('Hello Second Express!');
+    res.render('index.ejs');
   });
 };
--- secondexpress/server.js
+++ secondexpress/server.js
@@ -6,8 +6,14 @@ var aboutRouter = require("./routes/about.js")(app);
 
 
 app.set('port', process.env.PORT || 3030);
+// views for rendering
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+// 이렇게하거나 html모듈을 사용하거나
 
-
+// public for express static files
+app.use("/public", express.static(__dirname + "/public"));
 
 app.use(function(req, res) {
   res.type("text/plain");
