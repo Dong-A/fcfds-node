175e5acedaa3a310991e1aca2f71ed6de1a5913b
동아 <dongakim90@gmail.com>
2016-11-23 17:57:31 +0900

    chat

--- secondexpress/routes/chat.js
+++ secondexpress/routes/chat.js
@@ -1,5 +1,20 @@
 module.exports = function(app) {
+  var http = require("http").Server(app);
+  var io = require("socket.io")(http);
+
+
+
+
   app.get('/chat', function(req, res) {
-    res.send("This is chat");
+    res.render('chat.ejs');
   });
+
+  io.on("connection", function(socket) {
+    console.log("new user is connected.");
+  });
+
+  http.listen(3000, function() {
+    console.log("chat server is listen on 3000");
+  });
+
 };
